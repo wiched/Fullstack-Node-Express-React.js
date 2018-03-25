310c052433615564a62f95a09b42e3d092437cad
wiched <wiched@users.noreply.github.com>
2018-03-25 19:44:02 +0300

    env changes

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+mongoose.connect(keys.mongoURI);
+process.on('unhandledRejection', err => console.log(err.stack));
+
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-app.get("/", (req, res) => res.send("Hello Word!"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log("Example app on port 3000!"));
+app.listen(PORT, () => console.log('Example app on port 3000!'));
