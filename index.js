445374c04ebba8ed1dbb0a5bec85c7b443e3395f
wiched <wiched@users.noreply.github.com>
2018-03-26 22:43:12 +0300

    async await refactoring

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Example app on port 3000!'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Example app on port 5000!'));
